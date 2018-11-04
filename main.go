a503fab92fc2aec5268e2067d90924246dda3b5c
etnikg <etnik.gashi1@outlook.com>
2018-11-04 01:04:54 +0100

    Functionalize webServer-encryption

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"flag"
-	"fmt"
+	"html/template"
 	_ "image/gif"
 	_ "image/jpeg"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 // example encode usage: go run png-lsb-steg.go -operation encode -image-input-file test.png -image-output-file steg.png -message-input-file hide.txt
@@ -18,19 +23,45 @@ var operation = flag.String("op", "encode", "encode or decode")
 // the bitmask we will use (last two bits)
 var lsbMask = ^(uint32(3))
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		t, err := template.ParseFiles("front-end/stegano.html")
+		if err != nil {
+			http.Error(w, "Error processing template", http.StatusInternalServerError)
+		}
+		t.Execute(w, nil)
+
+	case http.MethodPost:
+		image := r.PostFormValue("file")
+		plText := r.FormValue("text")
+		r := bytes.NewReader([]byte(plText))
+		//using goquery to get value from plaintext element
+		doc, _ := goquery.NewDocumentFromReader(r)
+		text := doc.Find("textarea").Text()
+		encode(&image, &text)
+	default:
+		http.Error(w, "Not implemented", http.StatusNotImplemented)
+	}
+
+}
+
 // main, based on operation flag will encode data into image, or decode data from an image
 func main() {
 
 	// parse the command line options
-	flag.Parse()
+	// flag.Parse()
 
-	switch *operation {
-	case "encode":
-		fmt.Println("encoding!")
-		encode(inputFilename, messageFilename)
+	// switch *operation {
+	// case "encode":
+	// 	fmt.Println("encoding!")
+	// 	encode(inputFilename, messageFilename)
 
-	case "decode":
-		fmt.Println("decoding!")
-		decode(inputFilename)
-	}
+	// case "decode":
+	// 	fmt.Println("decoding!")
+	// 	decode(inputFilename)
+	// }
+
+	http.HandleFunc("/submit", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }

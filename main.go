a87d97b1985803a2b36d6fcbf926c1508eebcd5c
etnikg <etnik.gashi1@outlook.com>
2018-11-04 15:18:59 +0100

    Reading and Writing Image

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"bytes"
 	"flag"
+	"fmt"
 	"html/template"
+	"image"
 	_ "image/gif"
 	_ "image/jpeg"
+	"image/png"
+	"io"
 	"log"
 	"net/http"
-
-	"github.com/PuerkitoBio/goquery"
+	"os"
 )
 
 // example encode usage: go run png-lsb-steg.go -operation encode -image-input-file test.png -image-output-file steg.png -message-input-file hide.txt
@@ -33,13 +35,57 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		t.Execute(w, nil)
 
 	case http.MethodPost:
-		image := r.PostFormValue("file")
-		plText := r.FormValue("text")
-		r := bytes.NewReader([]byte(plText))
-		//using goquery to get value from plaintext element
-		doc, _ := goquery.NewDocumentFromReader(r)
-		text := doc.Find("textarea").Text()
-		encode(&image, &text)
+		url := "http://i.imgur.com/m1UIjW1.png"
+
+		response, e := http.Get(url)
+		if e != nil {
+			log.Fatal(e)
+		}
+
+		defer response.Body.Close()
+
+		//open a file for writing
+		file, err := os.Create("C:/Users/Etnik Gashi/Desktop/stegano1/tmp/asdf.png")
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Use io.Copy to just dump the response body to the file. This supports huge files
+		_, err = io.Copy(file, response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		file.Close()
+		//fmt.Println("Success!")
+		// Read image from file that already exists
+		existingImageFile, err := os.Open("C:/Users/Etnik Gashi/Desktop/stegano1/tmp/asdf.png")
+		if err != nil {
+			// Handle error
+			log.Fatal(err)
+		}
+		defer existingImageFile.Close()
+
+		// Calling the generic image.Decode() will tell give us the data
+		// and type of image it is as a string. We expect "png"
+		imageData, imageType, err := image.Decode(existingImageFile)
+		if err != nil {
+			// Handle error
+			log.Fatal(err)
+		}
+		fmt.Println(imageData)
+		fmt.Println(imageType)
+
+		// We only need this because we already read from the file
+		// We have to reset the file pointer back to beginning
+		existingImageFile.Seek(0, 0)
+
+		// Alternatively, since we know it is a png already
+		// we can call png.Decode() directly
+		loadedImage, err := png.Decode(existingImageFile)
+		if err != nil {
+			// Handle error
+			log.Fatal(err)
+		}
+		fmt.Println(loadedImage)
 	default:
 		http.Error(w, "Not implemented", http.StatusNotImplemented)
 	}

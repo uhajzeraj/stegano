55e79708d578a95fdcfdb12c8050998499530b13
Nehar Jashari <neharj@stud.ntnu.no>
2018-11-05 22:29:19 +0100

    Caesar's Cipher

---
+++ assets/js/caesarEncode.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $(`#upload-button`).click(function(e) {
+
+        e.preventDefault();
+
+        var formData = new FormData();
+        var plaintext = $(`textarea#plaintext`).val();
+        formData.append('plaintext', plaintext);
+
+        $('#loading').show();
+
+        $.ajax({
+            url: "caesar",
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                $('#loading').hide();
+                $(`textarea#ciphertext`).val(response);
+            }
+        });
+
+
+    });
+});

490d9a4f395097657ca036255e7997113e636682
besnikk@stud.ntnu.no <35761060+besnikkelmendi@users.noreply.github.com>
2018-11-13 12:31:00 +0100

    Admin functions added!

---
+++ admin/admin.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"regexp"
+
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
+	"strings"
+)
+
+var (
+	conn, _ = mgo.Dial("mongodb://admin:connecttome123@ds151533.mlab.com:51533/stegano")
+)
+
+type UserInfo struct {
+	User     string `bson:"user"`
+	HashPass string `bson:"passHash"`
+}
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/admin", adminHandler).Methods("POST")
+	router.HandleFunc("/admin/{user}", adminDeleteHandler).Methods("DELETE")
+
+	fmt.Print("Admin")
+
+}
+
+func adminDeleteHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	pathVars := mux.Vars(r)
+
+	if len(pathVars) != 1 {
+		http.Error(w, "400 - Bad Request, too many URL arguments.", http.StatusBadRequest)
+		return
+	}
+
+	err := conn.DB("stegano").C("user").Remove(bson.M{"user": pathVars["user"]})
+
+	if err != nil {
+		http.Error(w, "404 - User not found!", 404)
+		return
+	}
+
+	resp := "{\n"
+	resp += `"user":\n`
+	resp += pathVars["user"]
+	resp += "\n}"
+
+	fmt.Fprint(w, resp)
+}
+
+func adminHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	adminUser := &UserInfo{}
+
+	err := json.NewDecoder(r.Body).Decode(&adminUser)
+
+	if err != nil {
+		http.Error(w, err.Error(), 400) //checking for errors in the process and returning bad request if so
+		return
+	}
+
+	errorSlice := validateLogin(adminUser.User, adminUser.HashPass)
+
+	if len(errorSlice) > 0 {
+		for _, val := range errorSlice {
+			fmt.Println(val)
+		}
+		return
+	}
+	iter := conn.DB("stegano").C("user").Find(nil).Iter()
+
+	resp := "{\n"
+	resp += `"user":[\n`
+	for iter.Next(&adminUser) {
+		resp += adminUser.User
+		resp += ","
+	}
+	if err := iter.Close(); err != nil {
+		return
+	}
+	resp = strings.TrimRight(resp, ",")
+	resp += "]\n}"
+
+	fmt.Fprint(w,resp)
+
+}
+
+func validateLogin(user, pass string) []string {
+
+	var errorSlice []string
+
+	// Check if username is OK
+	match, err := regexp.MatchString(`^[a-zA-Z0-9_-]{6,30}$`, user)
+	returnEmptyError(err)
+	if !match {
+		errorSlice = append(errorSlice, "Username does not meet the requirements")
+	}
+
+	// Check if password is OK
+	match, err = regexp.MatchString(`^.{6,40}$`, pass)
+	returnEmptyError(err)
+	if !match {
+		errorSlice = append(errorSlice, "Password does not meet the requirements")
+	}
+
+	// Continue further if there are no errors
+	if len(errorSlice) == 0 {
+
+		result := UserInfo{}
+
+		conn.DB("stegano").C("admin").Find(bson.M{"user": user}).Select(bson.M{"user": user, "passHash": 1}).One(&result)
+
+		err := bcrypt.CompareHashAndPassword([]byte(result.HashPass), []byte(pass))
+		if err != nil {
+			errorSlice = append(errorSlice, "Username or password is not correct")
+		}
+	}
+
+	return errorSlice
+}
+
+func returnEmptyError(err error) {
+	if err != nil {
+		return
+	}
+}

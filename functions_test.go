58fddd1ab9067f9b1e7b3af45c76d41ac7cefe01
etnikg <etnik.gashi1@outlook.com>
2018-11-15 13:15:22 +0100

    Add some tests

---
+++ functions_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"testing"
+)
+
+var plainText = "helloooo"
+var cipherTxt = encodeCaesar(plainText, 3)
+
+func Test_encodeCaeser(t *testing.T) {
+	err := encodeCaesar(plainText, 3)
+	if err == "" {
+		t.Errorf("Error during Encode proces with Ceaser, %s", err)
+	}
+}
+
+func Test_decodeCaeser(t *testing.T) {
+	err := decodeCaesar(plainText, 3)
+	if err == "" {
+		t.Errorf("Error during Decode proces with Ceaser, %s", err)
+	}
+
+}
+func Test_errorPanic(t *testing.T) {
+	var err error
+	errorPanic(err)
+}
+func Test_exists(t *testing.T) {
+	boolValue, err := exists("./tmp/asdf.png")
+	if err != nil {
+		t.Error("Error:Doesn't exist")
+	}
+	if boolValue != true {
+		t.Error("Path dont exist")
+	}
+}
+
+func Test_validateSignup(t *testing.T) {
+
+	err := validateSignup("besniku7", "etnik123", "etnik123", "besnikk@stud.ntnu.no")
+	if err != nil {
+		t.Errorf("Error validating Signup:%s", err)
+	}
+}
+
+func Test_validateLogin(t *testing.T) {
+
+	err := validateLogin("etnik5", "etnik123")
+	if err == nil {
+		t.Errorf("Error validating Login:%s", err)
+	}
+}
+func Test_validateChangePassword(t *testing.T) {
+	err := validateChangePassword("etnik123", "gashi123", "gashi123", "Etnik5")
+	if err != nil {
+		t.Errorf("Error during Change Password, %s", err)
+	}
+}
+
+func Test_cipherEncoding(t *testing.T) {
+	err := cipher(plainText, -1, 3)
+	if err == "" {
+		t.Error("Error: Not encode!")
+	}
+	// if err != cipherTxt {
+	// 	t.Error("Encoding not correct")
+	// }
+}
+func Test_cipherDecoding(t *testing.T) {
+
+	err := cipher(cipherTxt, +1, 3)
+	if err == "" {
+		t.Errorf("Error: Not encode!, %s", err)
+	}
+	// if err != plainText {
+	// 	t.Error("Decoding not correct")
+	// }
+}
